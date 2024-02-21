54247c26cf9d5b84a856357f07ce2a5054317af3
¨David <davidl1199@hotmail.com>
2024-02-21 20:22:15 -0300

    feat: implementing validate register schema with zod.

--- todo-app-frontend/src/app/register/page.tsx
+++ todo-app-frontend/src/app/register/page.tsx
@@ -4,21 +4,20 @@ import { useState } from "react";
 import Image from "next/image";
 import signInSVG from '../../../public/sign-in.svg'
 import { useForm } from "react-hook-form";
-import { IRegister, initialFormValues } from "@/interface/IRegister";
+import { CreateRegisterSchema, RegisterSchema, initialFormValues } from "@/validations/validateRegisterForm";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 
 function Register() {
-  const [createAccSuccess, setCreateAccSuccess] = useState(false)
-  const {register, handleSubmit} = useForm({
+  const {register, handleSubmit, formState} = useForm<CreateRegisterSchema>({
+    resolver: zodResolver(RegisterSchema),
     mode: 'onSubmit',
     defaultValues: initialFormValues
   })
 
-  const handleRegister = async (data: IRegister) => {
-    const {name, email, password} = data
-    try {
-    
-      const response = await fetch('http://localhost:3001/register/', {
+  const handleRegister = async ({name, email, password}: CreateRegisterSchema) => {
+
+      const response = await fetch('http://localhost:3002/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -27,27 +26,18 @@ function Register() {
       });
 
       if (!response.ok) {
-        throw new Error('arrumar depois');
-      }
-
-      const responseData = await response.json();
-      
-      if (responseData.status === 'SUCCESS') {
-        setCreateAccSuccess(true)
+        alert('Erro interno do servidor.')
+        return
       }
       
+      alert('Conta criada com sucesso!')
 
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('Error:', error.message);
-      }
-    }
   };
   
   return (
     <section className="flex bg-rose-50 items-center justify-center flex-1 flex-col bg-sign-up-bg bg-cover bg-center h-screen " >
 
-      <div className="bg-black rounded-2xl px-24 py-12 bg-opacity-85">
+      <div className="bg-black rounded-2xl px-24 py-4 bg-opacity-85">
 
         <div className="flex justify-center">
           <Image src={signInSVG} className="mb-4" width={60} alt="Sign in SVG icon"/>
@@ -60,35 +50,58 @@ function Register() {
           <p className="text-slate-400 pb-10">and be able to enjoy your best Todo List!</p>
 
           <label className='flex flex-col gap-3 w-full text-white' htmlFor="name">Name:
-              <input {...register('name')} className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500 ' type= "text" name="name" id="name" placeholder='your name' />
+              <input
+                {...register('name')}
+                className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500 ' 
+                type= "text" 
+                name="name" 
+                id="name" 
+                placeholder='your name' 
+              />
           </label>
 
           <label className='flex flex-col gap-3 w-full text-white' htmlFor="email">Email:
-              <input {...register('email')} className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500' type="email" name="email" id="email" placeholder='example@email.com' />
+              <input 
+                {...register('email')} 
+                className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500'
+                name="email" 
+                id="email" 
+                placeholder='example@email.com' 
+              />
           </label>
 
           <label className='flex flex-col gap-3 w-full text-white' htmlFor="password">Password:
-              <input {...register('password')} className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500' type="password" name="password" id="password" placeholder='your secret pass' />
+              <input
+                {...register('password')} 
+                className='p-4 shadow-lg bg-slate-200 outline-none mb-8 rounded-md text-black  placeholder:text-gray-500' 
+                type="password" 
+                name="password" 
+                id="password" 
+                placeholder='your secret pass' 
+               />
           </label>
 
-          <button className='font-semibold shadow-lg mt-4 text-white hover:bg-emerald-700 transition-all bg-emerald-600 py-4 px-8 rounded-md w-full' type="submit">Criar conta</button>
+          <button
+            className='font-semibold shadow-lg mt-4 text-white hover:bg-emerald-700 transition-all bg-emerald-600 py-4 rounded-md w-full'
+            type="submit">Criar conta
+          </button>
 
-        </form>
-
-        {createAccSuccess ? (
-          <div className="flex justify-center mt-14 gap-1">
-            <p className="text-white">Conta criada com sucesso!</p>
-            <a className="text-emerald-600" href="/login">Logue aqui.</a>
-          </div>
-        ): (
-          <div className='flex justify-center mt-14 gap-1'>
+          <div className='flex justify-center gap-1 mt-6'>
             <p className="text-white">Já tem uma conta?</p>
             <button>
               <a className='text-emerald-600' href="/login">Entre!</a>
             </button>
           </div>
-        )}
 
+        </form>
+
+        {formState.errors && (
+                <p className='text-sm text-red-400 mt-4'>{
+                  formState.errors.password?.message ||
+                  formState.errors.email?.message    ||
+                  formState.errors.name?.message}
+                </p>
+              )}
       </div>
 
     </section>
---
+++ todo-app-frontend/src/validations/validateRegisterForm.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+export const initialFormValues = {
+  name: '',
+  email: '',
+  password: ''
+}
+
+export const RegisterSchema = z.object({
+  name: z.string().min(3, {message: 'Mínimo de 3 caracteres.'}),
+  email: z.string().email({message: 'Email incorreto. Ex: joao@gmail.com'}),
+  password: z.string().min(8, { message: 'Senha deve ter no mínimo 8 caracteres.' }),
+})
+
+export type CreateRegisterSchema = z.infer<typeof RegisterSchema>
