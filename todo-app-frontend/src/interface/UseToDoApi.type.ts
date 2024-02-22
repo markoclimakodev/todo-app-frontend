2e371c6198cb1409a3807a15e049b89b78435366
markoclimako <mkclimako@gmail.com>
2024-02-22 00:50:20 -0300

    feat(interfaces): Define API types for data and requests
    - Created interfaces to model API interactions:
        - `Method`: Represents HTTP methods for API calls.
        - `ReqData`: Defines expected data formats for registration and login requests.
        - `QueryOptions`: Specifies filter options for task queries.
        - `TodoTaskParams`: Encapsulates parameters for API calls.
        - `LoginResponse`: Captures the structure of login responses.
        - `ApiResponse`: Represents the general format of API responses.

---
+++ todo-app-frontend/src/interface/UseToDoApi.type.ts
@@ -0,0 +1,26 @@
+import { ILogin } from "./ILogin"
+import { IRegister } from "./IRegister"
+import { Todo } from "./ITodo"
+
+export type Method = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE'
+export type ReqData = IRegister | ILogin
+export type QueryOptions = 'completed' | ''
+export type TodoTaskParams = {
+    endpoint: string,
+    method: Method,
+    token?: string
+    reqData?: ReqData
+}
+
+export interface LoginResponse {
+    authResponse: {
+        token: string
+        userId: string
+    }
+}
+
+export interface ApiResponse {
+    success: boolean;
+    data: LoginResponse | Todo[];
+    error?: string | undefined;
+}
