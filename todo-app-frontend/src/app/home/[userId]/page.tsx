16ee84e63747caa7d174240dfe45ce49afb835dc
markoclimako <mkclimako@gmail.com>
2024-02-22 00:58:55 -0300

    refactor(Todos): Integrate useTodoApi for data fetching and state management
    - Replaced `useFetch` with `useTodoApi` for API calls, aligning with shared logic.
    - Introduced local state management for pending and completed todos using `useState`.
    - Moved data fetching and filtering logic into a `fetchTodos` function for better organization.
    - Optimized `useEffect` dependencies for efficient re-fetching.

--- todo-app-frontend/src/app/home/[userId]/page.tsx
+++ todo-app-frontend/src/app/home/[userId]/page.tsx
@@ -1,20 +1,49 @@
 'use client'
 import Sidebar from "@/components/Sidebard/Sidebar"
-import { Todo } from "@/interface/Todo"
 import { usePathname } from "next/navigation"
 import { MdOutlineHome, } from "react-icons/md"
-import { useFetch } from "@/hooks/useFetch"
 import TodoCard from "./TodoCard"
 import CreateTodo from "./CreateTodo"
+import { useTodoApi } from "@/hooks/useTodoApi"
+import { useCallback, useEffect, useState } from "react"
+import { Todo } from "@/interface/ITodo"
 
 export default function Todos() {
     const token = localStorage.getItem('token') || ''
     const endpoint = usePathname()
-    const todos:Todo[] = useFetch(endpoint,'GET',token)
+    const { apiResponse, todoTask } = useTodoApi('http://localhost:3002')
+    const [todos, setTodos] = useState<Todo[]>([])
+    const [completedTodos, setCompletedTodos] = useState<Todo[]>([])
+
+    const fetchTodos = useCallback( async () => {
+        try {
+            await todoTask({
+                endpoint,
+                method: 'GET',
+                token,
+            });
+
+            if (apiResponse && apiResponse.data) {
+                const allTodos = apiResponse.data as Todo[];
+                const pendingTodos = allTodos.filter((todo) => !todo.completed);
+                const completedTodos = allTodos.filter((todo) => todo.completed);
+        
+                setTodos(pendingTodos);
+                setCompletedTodos(completedTodos);
+            }
+        } catch (error) {
+            console.error("Error fetching todos:", error);
+        }
+    },[apiResponse, endpoint, todoTask, token])
+
+
+    useEffect(() => {
+        fetchTodos();
+    }, [fetchTodos]); 
 
     return (
         <main className="flex">
-            
+
             <Sidebar />
 
             <section className="bg-slate-100 w-full p-5">
@@ -25,15 +54,20 @@ export default function Todos() {
 
                 <section className="w-full flex flex-col px-8">
                     {
-                        todos.map((todo) => <TodoCard key={todo.id} todo={todo}/>)
+                        todos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
                     }
                 </section>
 
-                <div className="p-8 text-xl font-bold text-gray-700">
+                <section className="p-8 text-xl font-bold text-gray-700">
                     <details>
                         <summary className="list-outside ml-7 pl-2 hover:cursor-pointer">Concluídas</summary>
+                        <ul>
+                        {
+                        completedTodos.map((todo) => <TodoCard key={todo.id} todo={todo} />)
+                    }
+                        </ul>
                     </details>
-                </div>
+                </section>
 
             </section>
         </main>
