e0303b5b251d240e2c0d7ba40b7ebaad3265654b
¨David <davidl1199@hotmail.com>
2024-02-22 21:43:12 -0300

    feat: implements deleteTodo method and function + perf in openUpdateModal.

--- todo-app-frontend/src/app/home/[userId]/TodoCard.tsx
+++ todo-app-frontend/src/app/home/[userId]/TodoCard.tsx
@@ -1,11 +1,13 @@
 import { ITodo } from "@/interface/ITodo"
-import { MdBookmark } from "react-icons/md"
+import { MdBookmark, MdEditNote, MdStarBorder } from "react-icons/md"
 import { useState } from "react";
-import TodoAction from "./TodoActions";
-import { actionButtons } from "./data";
 import { formatDateTime } from "@/helpers/formatDateTime";
 import { extractStringsWithPosition } from "@/helpers/extractStringsWithPosition";
 import TodoDescription from "./TodoDescription";
+import UpdateTodoModal from "./UpdateTodoModal";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { useFetch } from "@/hooks/useFetch";
+import { useAuth } from "@/hooks/useToken";
 
 
 interface TodoCardProps {
@@ -15,32 +17,59 @@ interface TodoCardProps {
 function TodoCard({ todo }: TodoCardProps) {
   const { id, title, description, createdAt, updatedAt } = todo
   const [openTodo, setOpenTodo] = useState(false)
+  const [openUpdateTodoModal, setOpenUpdateModal] = useState(false)
 
-  const handleTodo = () => {
+  const { token, userId } = useAuth()
+
+  const { todoTask } = useFetch('http://localhost:3002/')
+
+  const handleDeleteTodo = async () => {
+    await todoTask({
+      endpoint: `home/${userId}/${todo.id}`, 
+      method: 'DELETE',
+      token,
+    })
+  }
+
+  const handleOpenTodo = () => {
     setOpenTodo(!openTodo)
+  }
+
+  const handleOpenUpdateModal = () => {
+    setOpenUpdateModal(!openUpdateTodoModal)
   }
 
   const descriptionParts = extractStringsWithPosition(description)
 
-
-
   return (
     <section className="flex flex-col bg-white p-2 rounded-md py-5 px-8 gap-4 mt-5 hover:bg-blue-100 transition-all cursor-pointer" key={id} id={id} >
 
+      < UpdateTodoModal openUpdateTodoModal={openUpdateTodoModal} todoId={id} closeUpdateTodoModal={handleOpenUpdateModal}/>
+
       <section className="text-lg text-gray-700 flex w-full justify-between pl-1 ">
-        <section className="flex items-center w-full" onClick={handleTodo}>
+
+        <section className="flex items-center w-full" onClick={handleOpenTodo}>
           <MdBookmark />
           <span className="cursor-pointer">{title}</span>
         </section>
+
         <section className="flex items-center gap-1">
-          {
-            actionButtons.map((action) => <TodoAction icon={action.icon} key={action.id} id={action.id} />)
-          }
+
+          <button onClick={handleOpenUpdateModal}>
+            <MdEditNote  title="Editar Tarefa" className="cursor-pointer size-6 hover:scale-150 transition-all" color="rgb(59 130 246)" />
+          </button>
+          <button>
+            <MdStarBorder title="Adicionar à lista de importantes" color="rgb(59 130 246)" className="cursor-pointer size-6 hover:scale-150 transition-all" />
+          </button>
+          <button onClick={handleDeleteTodo}>
+            <FaRegTrashAlt title="Excluir Tarefa"  className="cursor-pointer size-4 hover:scale-150 transition-all" color="rgb(59 130 246)" />
+          </button>
+
         </section>
 
       </section>
 
-      <section className={openTodo ? 'transition ease-out duration-300 transform flex flex-col gap-4' : 'hidden'} onClick={handleTodo}>
+      <section className={openTodo ? 'transition ease-out duration-300 transform flex flex-col gap-4' : 'hidden'} onClick={handleOpenTodo}>
 
     <TodoDescription descriptionParts={descriptionParts} description={description} />
         <footer className="px-3  pt-3 flex w-fit flex-col text-[10px] text-gray-600 text-start">
