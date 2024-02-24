822914b67f18fb8e0f9dd0b33c3b886b5f3deb4b
markoclimako <mkclimako@gmail.com>
2024-02-24 15:14:25 -0300

    feat: add categories field to Todos model

--- todo-app-backend/src/external/prisma/migrations/20240224173213_/migration.sql
+++ todo-app-backend/src/external/prisma/migrations/20240224173213_/migration.sql
@@ -1,4 +1,16 @@
 -- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "todos" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
@@ -6,9 +18,13 @@ CREATE TABLE "todos" (
     "user_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
 ALTER TABLE "todos" ADD CONSTRAINT "todos_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ todo-app-backend/src/external/prisma/migrations/20240224180737_update_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "todos" ALTER COLUMN "categories" SET DEFAULT 'todo';
