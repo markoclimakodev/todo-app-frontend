e65896c3ee4ba4ce15b71b49b662edd779f39051
markoclimako <mkclimako@gmail.com>
2024-03-06 14:10:17 -0300

    refactor: refactor user data to remove unused task lists
    * Removed the `taskLists` property from the users object.
    * Improves data structure and maintainability.

--- todo-app-backend/src/external/prisma/seed/data/users.seed.ts
+++ todo-app-backend/src/external/prisma/seed/data/users.seed.ts
@@ -1,38 +1,17 @@
 import { PasswordHash } from '../../../../utils/passwordHash'
-const defaultCategories = [ 'todas tarefas' , 'importantes' , 'concluídas' ]
-const userDavidDefaultCategories = [
-	...defaultCategories ,
-	'viagens' ,
-	'livros' ,
-	'criativos' ,
-	'casa' ,
-	'esportes' ,
-	'idiomas'
-]
-
-const userMarkoDefaultCategories = [
-	...defaultCategories ,
-	'profissional' ,
-	'code' ,
-	'academia' ,
-	'financeiro' ,
-	'saúde e bem-estar'
-]
 
 export const users = [
 	{
-		id        : '5ad558d3-12af-4852-9243-d5f92311b06c' ,
-		name      : 'David' ,
-		email     : 'david@gmail.com' ,
-		password  : PasswordHash.hash( 'coxinha123' ) ,
-		taskLists : userDavidDefaultCategories
+		id       : '5ad558d3-12af-4852-9243-d5f92311b06c' ,
+		name     : 'David' ,
+		email    : 'david@gmail.com' ,
+		password : PasswordHash.hash( 'coxinha123' ) ,
 	} ,
 	{
-		id        : 'ccf7af04-7c83-4e2f-8b4a-1420d2b01f5c' ,
-		name      : 'Marko' ,
-		email     : 'marko@gmail.com' ,
-		password  : PasswordHash.hash( 'frango123' ) ,
-		taskLists : userMarkoDefaultCategories
+		id       : 'ccf7af04-7c83-4e2f-8b4a-1420d2b01f5c' ,
+		name     : 'Marko' ,
+		email    : 'marko@gmail.com' ,
+		password : PasswordHash.hash( 'frango123' ) ,
 	}
 ]
 
--- todo-app-backend/src/external/prisma/seed/seed.ts
+++ todo-app-backend/src/external/prisma/seed/seed.ts
@@ -1,22 +1,15 @@
 import { PrismaClient } from '../../../../node_modules/.prisma/client/index'
-import { todos } from './data/todos.seed'
 import { users } from './data/users.seed'
 
 const prisma = new PrismaClient()
 
 async function seeedDatabase () {
 	try {
-		const createUsers = await prisma.users.createMany({
+		const createUsers = await prisma.user.createMany({
 			data : users
 		})
 
-		const createTodos = await prisma.todos.createMany({
-			data : todos
-		})
-
 		console.log( 'Usuários cadastrados' , createUsers )
-
-		console.log( 'Todos cadastrados' , createTodos )
 
 		await prisma.$disconnect()
 	} catch ( error ) {
