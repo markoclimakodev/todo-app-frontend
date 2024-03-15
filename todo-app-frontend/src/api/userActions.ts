541a329af73ffbd8fa874fa6cd921711a0c83dee
markoclimako <mkclimako@gmail.com>
2024-03-15 16:15:56 -0300

    feat(auth): implement user login and registration with auth storage
    - Introduces `userLogin` function to authenticate users and save tokens.
    - Implements `userRegister` function for user registration.
    - Utilizes `saveAuth` to store retrieved tokens and user ID for persistence.
    - Employs interfaces `ILogin`, `ILoginResponse`, and `IRegister` for type safety.

---
+++ todo-app-frontend/src/api/userActions.ts
@@ -0,0 +1,40 @@
+import { ILogin } from "@/interface/ILogin";
+import { ILoginResponse } from "@/interface/user/ILoginResponse";
+import { saveAuth } from "./auth";
+import { IRegister } from "@/interface/IRegister";
+
+
+export async function userLogin({ email, password }: ILogin): Promise<void> {
+
+    const response = await fetch(`http://localhost:3002/user/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password })
+    });
+
+    if (!response.ok) {
+        throw new Error("Error while trying to login in");
+    }
+
+    const data = await response.json() as ILoginResponse;
+    const { token, userId } = data.authResponse;
+
+    saveAuth({ token, userId });
+}
+
+export async function userRegister({ name,email, password }: IRegister): Promise<void> {
+
+    const response = await fetch(`http://localhost:3002/user/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name,email, password })
+    });
+
+    if (!response.ok) {
+        throw new Error("Error while trying to register");
+    }
+}
