c11a4b8ed62ca9f5b00ae398e3c44f762e987542
markoclimako <mkclimako@gmail.com>
2024-03-15 16:57:58 -0300

    feat: update seed

--- todo-app-backend/src/external/prisma/seed/data/users.seed.ts
+++ todo-app-backend/src/external/prisma/seed/data/users.seed.ts
@@ -1,17 +1,9 @@
 import { PasswordHash } from '../../../../utils/passwordHash'
 
-export const users = [
-	{
-		id       : '5ad558d3-12af-4852-9243-d5f92311b06c' ,
-		name     : 'David' ,
-		email    : 'david@gmail.com' ,
-		password : PasswordHash.hash( 'coxinha123' ) ,
-	} ,
-	{
-		id       : 'ccf7af04-7c83-4e2f-8b4a-1420d2b01f5c' ,
-		name     : 'Marko' ,
-		email    : 'marko@gmail.com' ,
-		password : PasswordHash.hash( 'frango123' ) ,
-	}
-]
+export const users = {
+	id       : '5ad558d3-12af-4852-9243-d5f92311b06c' ,
+	name     : 'David' ,
+	email    : 'david@gmail.com' ,
+	password : PasswordHash.hash( 'coxinha123' ) ,
+}
 
--- todo-app-backend/src/external/prisma/seed/seed.ts
+++ todo-app-backend/src/external/prisma/seed/seed.ts
@@ -1,13 +1,38 @@
+/* eslint-disable max-lines-per-function */
 import { PrismaClient } from '../../../../node_modules/.prisma/client/index'
 import { users } from './data/users.seed'
 
 const prisma = new PrismaClient()
 
 async function seeedDatabase () {
+	const defaultUserCategories = [ 'todas' , 'importantes' ]
 	try {
-		const createUsers = await prisma.user.createMany({
+		const createUsers = await prisma.user.create({
 			data : users
 		})
+
+		defaultUserCategories.forEach( async ( category ) => {
+			await prisma.userCategory.create({
+				data : {
+					category : {
+						connectOrCreate : {
+							where : {
+								name : category
+							} ,
+							create : {
+								name : category
+							} ,
+						} ,
+					} ,
+					user : {
+						connect : {
+							id : users.id ,
+						} ,
+					} ,
+				} ,
+			})
+		}
+		)
 
 		console.log( 'Usuários cadastrados' , createUsers )
 
