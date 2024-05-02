e55c3d6950b0c65e87c6a5bf826d5c855e24ccde
markoclimako <mkclimako@gmail.com>
2024-05-02 00:33:39 -0300

    refactor: apiUrl

--- src/api/apiURL.ts
+++ src/api/apiURL.ts
@@ -1,1 +1,3 @@
-export const API_URL = `${process.env.REACT_APP_API_PROTOCOL}://${process.env.REACT_APP_API_HOST}`
+export const HOST = process.env.REACT_APP_API_HOST || "localhost:3001";
+export const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || "http";
+export const API_URL = `${PROTOCOL}://${HOST}`;
